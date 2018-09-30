fd1d048fea6a69be5689b55028095dc943c31a8d
RodrigoGustavo <rodrigogustavo@gmail.com>
2018-09-30 19:21:52 -0400

    Side Drawer and Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/_Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component{
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout;
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer:false});
+    }
+
+    render(){
+        return(
+            <Aux>
+                <Toolbar/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/_Aux'
 import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
 
     return (
-        <div className={classes.SideDrawer}>
+        <Aux>
+        <Backdrop clicked={props.closed} show={props.open}/>
+        <div className={attachedClasses.join(' ')}>
             <Logo height='11%'/>
             <nav>
                 <NavigationItems/>
             </nav>
         </div>
+        </Aux>
     );
 };
 
